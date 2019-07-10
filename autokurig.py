3d17b57b863bafef8254b1ee311c74ae56bf7d23
Dustin Johnson <eventuallyiwin@gmail.com>
2019-07-09 20:36:20 -0500

    asdfa

--- autokurig.py
+++ autokurig.py
@@ -4,6 +4,22 @@ try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 mode = GPIO.getmode()
-print(mode)
+
+# set channel
+# GPIO.setup(channel, GPIO.IN)
+# GPIO.setup(channel, GPIO.OUT)
+# GPIO.setup(channel, GPIO.OUT, initial=GPIO.HIGH)
+# read channel
+# GPIO.input(channel)
+# GPIO.output(channel, state)
+# State can be 0 / GPIO.LOW / False or 1 / GPIO.HIGH / True.
+
+GPIO.setup(3, GPIO.OUT)
+GPIO.output(3,1)
+
+GPIO.cleanup()
