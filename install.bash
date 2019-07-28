ce0870b3c08aefd0819fa6231f951bd0509073fe
Dustin Johnson <eventuallyiwin@gmail.com>
2019-07-28 16:08:57 -0500

    asdf

--- install.bash
+++ install.bash
@@ -8,5 +8,5 @@ cd $APPDIR
 sudo git pull
 sudo cp $APPDIR/$APPNAME.conf /etc/apache2/sites-available/$APPNAME.conf
 echo "copied $APPNAME.conf to /etc/apache2/sites-available/$APPNAME.conf"
-sudo service apache2 restart
+# sudo service apache2 restart
 # sudo a2ensite /etc/apache2/sites-available/$APPNAME.conf
