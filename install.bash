d21981b6293c521c5ecd7924fb624bb5612d0d97
Dustin Johnson <eventuallyiwin@gmail.com>
2019-07-28 15:26:39 -0500

    asdf

--- install.bash
+++ install.bash
@@ -5,7 +5,7 @@ echo $APPNAME
 APPDIR="/home/pi/$APPNAME"
 echo $APPDIR
 cd $APPDIR
-# git pull
-# sudo mv $APPDIR/$APPNAME.conf /etc/apache2/sites-available/$APPNAME.conf
-# sudo service apache2 restart
+sudo git pull
+sudo mv $APPDIR/$APPNAME.conf /etc/apache2/sites-available/$APPNAME.conf
+sudo service apache2 restart
 # sudo a2ensite /etc/apache2/sites-available/$APPNAME.conf
