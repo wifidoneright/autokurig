83ad55cb5ef82a158458482dd0279038fff7ccd0
Dustin Johnson <eventuallyiwin@gmail.com>
2019-07-28 15:12:52 -0500

    asdf

--- install.bash
+++ install.bash
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 APPNAME = "autokurig"
-APPDIR = "/home/pi/" + APPNAME
-cd APPDIR
+APPDIR = "/home/pi/$APPNAME"
+cd $APPDIR
 git pull
-sudo mv /home/pi/autokurig/autokurig.conf /etc/apache2/sites-available/autokurig.conf
+sudo mv $APPDIR/$APPNAME.conf /etc/apache2/sites-available/$APPNAME.conf
 sudo service apache2 restart
-# sudo a2ensite /etc/apache2/sites-available/autokurig.conf
+# sudo a2ensite /etc/apache2/sites-available/$APPNAME.conf
